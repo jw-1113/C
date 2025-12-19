9a65d8691c96db370f16b5f8e781298590b572b5
jw-1113 <densang1113@gmail.com>
2025-12-19 15:59:44 +0900

    Add main function with ap2 initialization
    Initial implementation of main function with ap2 initialization and main call.

---
+++ atmega/20251219.c
@@ -0,0 +1,27 @@
+#include "ap2.h"
+
+   
+int main(){
+//     ap1Init();
+//     ap1Main();
+//
+        ap2Init();
+        ap2Main();
+
+    // 순서대로 켜지는 코드
+    // DDRD = 0xff;
+    // PORTD = 0x00;   // 포트 Ddml 8비트에 전부 0을 넣어 LOW로 만듬
+
+    //  while (1) {
+    //     for ( uint8_t i = 0; i<8; i++){
+    //         PORTD = (1<<i);
+    //         _delay_ms(300);
+
+    //     }
+        // PORTD = 0xaa; // 1010 1010
+        // _delay_ms(500);
+        // PORTD = 0x55; // 0101 0101
+        // _delay_ms(500);
+
+    //}
+}
