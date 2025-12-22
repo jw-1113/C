b0fd12929515dc4705b853eb18a66ec3d7e8a1f6
jw-1113 <densang1113@gmail.com>
2025-12-22 16:57:07 +0900

    Add files via upload

--- driver/led.c
+++ driver/led.c
@@ -9,18 +9,39 @@ void ledInint(LED *led){
     // (led->port - 1)를 이용해서 PORT에서 DDR로 접근
     // (1 << led->pinNumber)와 OR연산을 통해서 지정된 포트를 출력
 
-    *(led->port - 1) |= (1 << led->pinNumber);
-    // *(led->ddr) |= (1 << led->pinNumber); 위와 같은 코드
+    *(led->ddr) |= (1 << led->pinNumber);
 }
 
 
 // LED On 함수
 void ledOn(LED *led){
     *(led->port) |= (1 << led-> pinNumber);
-
 }
 
 // LED Off 함수
 void ledOff(LED *led){
     *(led->port) &= ~(1 << led->pinNumber);
+}
+
+// LED포트를 출력모드로 사용하기위해 초기설정을 하는 함수 정의
+void ledInit(){   // 포트설정만 하면 되기 때문에 인자값이 없이 함수 정의
+    DDRD = 0xff;    // 출력으로 설정하기 위해 1111 1111 대입
+}
+
+// LED포트(PORTD)에 데이터를 출력하기 위해 함수를 정의
+void GPIO_Output(uint8_t data){
+    PORTD = data;   // 인자값을 넘겨준 data를 PORTD에 대입
+}
+
+// LED 데이터를 좌측으로 이동시키는 함수 정의
+void ledLeftShift(uint8_t *data){
+    // 역참조(*)를 사용하여 포인터 data가 가르키는 곳의 값을 사용
+    *data = (*data >> 7) | (*data << 1); // 좌측으로 1비트 이동
+    GPIO_Output(*data); // 포인터가 가르키는 data의 값을 넘겨줌
+}
+
+// LED 데이터를 우측으로 이동시키는 함수 정의
+void ledRightShift(uint8_t *data){
+    *data = (*data << 7) | (*data >> 1);
+    GPIO_Output(*data); 
 }
--- driver/led.h
+++ driver/led.h
@@ -1,9 +1,17 @@
 // include 와 함수의 원형을 분리
+// - define 함수의 원형 선언으로 구성되어 있고
+// - 함수에서 
+
+
 #include "common/def.h"
+
+#define LED_DDR DDRD
+#define LED_PORT PORTD
 
 typedef struct
 {
     volatile uint8_t    *port;
+    volatile uint8_t    *ddr;
     uint8_t             pinNumber;
 }LED;
 
@@ -11,3 +19,9 @@ typedef struct
 void ledInint(LED *led);
 void ledOn(LED *led);
 void ledOff(LED *led);
+
+// ledmove를 위한 함수
+void ledInit();
+void GPIO_Output(uint8_t data);
+void ledLeftShift(uint8_t *data);
+void ledRightShift(uint8_t *data);
